b5db747df06c34212cc254f98e7e47126e081016
Alex Usmanov <mr.alex.usmanov@gmail.com>
2024-07-26 13:12:48 +0500

    increase buffer size and use nil format

--- packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
+++ packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -122,8 +122,11 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
 
       let input = engine.inputNode
       let bus = 0
+        
+      let inputFormat = input.inputFormat(forBus: bus);
+      let bufferSize = AVAudioFrameCount(inputFormat.sampleRate * 2)
 
-      input.installTap(onBus: bus, bufferSize: 22050, format: input.inputFormat(forBus: bus)) {
+      try input.installTap(onBus: bus, bufferSize: bufferSize, format: nil) {
         buffer, _ -> Void in
         let samples = buffer.floatChannelData?[0]
         // audio callback, samples in samples[0]...samples[buffer.frameLength-1]
